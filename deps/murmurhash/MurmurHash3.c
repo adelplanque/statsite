7d00816989ead0370ad35eeb7635fe402665f114
adelplanque <alaindelplanque@mailoo.org>
2026-01-30 10:56:59 +0100

    build: append meson.build

--- deps/murmurhash/MurmurHash3.c
+++ deps/murmurhash/MurmurHash3.c
@@ -14,7 +14,7 @@
 
 #define	FORCE_INLINE inline __attribute__((always_inline))
 
-inline uint64_t rotl64(uint64_t x, int8_t r){
+FORCE_INLINE uint64_t rotl64(uint64_t x, int8_t r){
   return (x << r) | (x >> (64 - r));
 }
 
